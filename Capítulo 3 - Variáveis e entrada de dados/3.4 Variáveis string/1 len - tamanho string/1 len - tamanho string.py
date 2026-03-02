0a91a06976a4280e2bedb5fd0ae57fc2f9e1df10
Filipe Leal <filipelealtec@gmail.com>
2026-03-02 16:50:49 -0300

    C/C++ 2025

---
+++ Capítulo 3 - Variáveis e entrada de dados/3.4 Variáveis string/1 len - tamanho string/1 len - tamanho string.py
@@ -0,0 +1,10 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> len("A")
+1
+>>> len("AB")
+2
+>>> len("")
+0
+>>> len("O rato roeu a roupa")
+19
